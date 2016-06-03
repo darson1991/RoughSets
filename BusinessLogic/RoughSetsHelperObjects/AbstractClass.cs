6a9df876dc4c460a796d7ca211bf8a911ce4a6ea
Dariusz Świętek <dariusz.swietek@gmail.com>
2016-06-03 20:34:54 +0200

    added rough sets helper objects

---
+++ BusinessLogic/RoughSetsHelperObjects/AbstractClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace BusinessLogic.RoughSetsHelperObjects
+{
+    public class AbstractClass
+    {
+        public List<int> ObjectsIndexes { get; set; }
+        public List<double> ArgumentsValues { get; set; }
+        public bool IsClear { get; set; }
+        public double? Decision { get; set; }
+
+        public AbstractClass()
+        {
+            ObjectsIndexes = new List<int>();
+            ArgumentsValues = new List<double>();
+            IsClear = true;
+        }
+    }
+}
---
+++ BusinessLogic/RoughSetsHelperObjects/Reduct.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace BusinessLogic.RoughSetsHelperObjects
+{
+    public class Reduct
+    {
+        public List<int> Subset { get; set; } 
+        public List<AbstractClass> AbstractClasses { get; set; }
+        public List<DataObject> DataObjects { get; set; }
+        public double Approximation { get; set; }
+
+        public Reduct()
+        {
+            Subset = new List<int>();
+            AbstractClasses = new List<AbstractClass>();
+            DataObjects = new List<DataObject>();
+        } 
+    }
+}
