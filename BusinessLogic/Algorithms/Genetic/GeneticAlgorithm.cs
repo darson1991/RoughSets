fc222c6f3a0dd91816e4a641451b196e23d99fbe
Dariusz Świętek <dariusz.swietek@gmail.com>
2016-06-10 08:47:09 +0200

    start genetic algorithm

--- BusinessLogic/Algorithms/Genetic/GeneticAlgorithm.cs
+++ BusinessLogic/Algorithms/Genetic/GeneticAlgorithm.cs
@@ -2,6 +2,26 @@
 {
     public class GeneticAlgorithm: BaseAlgorithm
     {
+        public double MutationPossibility { get; set; }
+        public double CrossingOverPossibility { get; set; }
+        public int TurnamentSize { get; set; }
+        public int PopulationSize { get; set; }
+        public int IterationWithoutImprovment { get; set; }
+
+        public GeneticAlgorithm()
+        {
+            SetInitValues();
+        }
+
+        private void SetInitValues()
+        {
+            MutationPossibility = 0.01;
+            CrossingOverPossibility = 0.5;
+            TurnamentSize = 5;
+            PopulationSize = 10;
+            IterationWithoutImprovment = 300;
+        }
+
         public override void Calculate()
         {
             throw new System.NotImplementedException();
---
+++ BusinessLogic/Algorithms/Genetic/Population.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using BusinessLogic.Algorithms.Common;
+
+namespace BusinessLogic.Algorithms.Genetic
+{
+    public class Population
+    {
+        public List<Reduct> Individuals { get; set; } 
+
+        public Population()
+        {
+            Individuals = new List<Reduct>();
+        }
+
+    }
+}
