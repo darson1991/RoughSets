77d94b8e8854a924faf3023f9b2fea496d6666a7
Dariusz Świętek <dariusz.swietek@gmail.com>
2016-06-23 05:57:51 +0200

    added input values properties for bees colony algorithm

--- BusinessLogic/Algorithms/Bees/BeesColonyAlgorithmInputValues.cs
+++ BusinessLogic/Algorithms/Bees/BeesColonyAlgorithmInputValues.cs
@@ -2,5 +2,10 @@
 {
     public class BeesColonyAlgorithmInputValues : BaseAlgorithmInputValues
     {
+        public int PopulationSize { get; set; }
+        public int NumberOfEliteSolutions { get; set; }
+        public int NumberOfBestSolutions { get; set; }
+        public int EliteNeighborhoodSize { get; set; }
+        public int BestNeighborhoodSize { get; set; }
     }
 }
